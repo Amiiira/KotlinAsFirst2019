3afd1daa9e2df1d78bc54bbda22c62442ee78714
Amiiira <zuhir.as@edu.spbstu.ru>
2019-10-09 22:07:52 +0300

    Lesson 2 task 1&2 corrected !

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -54,7 +54,7 @@ fun daysInMonth(month: Int, year: Int): Int {
         month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 -> 31
         month == 4 || month == 6 || month == 9 || month == 11 -> 30
         ((year % 400 == 0) || (year % 100 != 0)) && (year % 4 == 0) -> 29
-        else -> 27
+        else -> 28
     }
 }
 
