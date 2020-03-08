64ff1c63aca65a9d25139550c679456318161f27
Amiiira <zuhir.as@edu.spbstu.ru>
2020-03-08 20:27:33 +0300

    Complex done

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -2,6 +2,9 @@
 
 package lesson11.task1
 
+import lesson1.task1.sqr
+import java.lang.IllegalArgumentException
+
 /**
  * Класс "комплексое число".
  *
@@ -16,45 +19,62 @@ class Complex(val re: Double, val im: Double) {
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(TODO(), TODO())
+    constructor(x: Double) : this(x, 0.0)
 
     /**
      * Конструктор из строки вида x+yi
      */
-    constructor(s: String) : this(TODO(), TODO())
+    constructor(s: String) : this(
+        s.split("-", "+")[0].toDouble(),
+        if (s.split("-").size == 1) (s.split("+")[1].split("i")[0].toDouble())
+        else (-s.split("-")[1].split("i")[0].toDouble())
+    )
 
     /**
      * Сложение.
      */
-    operator fun plus(other: Complex): Complex = TODO()
+    operator fun plus(other: Complex) = Complex(re + other.re, im + other.im)
 
     /**
      * Смена знака (у обеих частей числа)
      */
-    operator fun unaryMinus(): Complex = TODO()
+    operator fun unaryMinus(): Complex = Complex(re * -1, im * -1)
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Complex): Complex = TODO()
+    operator fun minus(other: Complex): Complex = Complex(re - other.re, im - other.im)
 
     /**
      * Умножение
      */
-    operator fun times(other: Complex): Complex = TODO()
+    operator fun times(other: Complex): Complex = Complex(re * other.re - im * other.im, im * other.re + re * other.im)
 
     /**
      * Деление
      */
-    operator fun div(other: Complex): Complex = TODO()
+    operator fun div(other: Complex): Complex {
+        val mul = sqr(other.re) + sqr(other.im)
+        return Complex(((re * other.re) + (im * other.im)) / mul, ((im * other.re) - (re * other.im)) / mul)
+    }
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (other is Complex && (re == other.re) && (im == other.im)) return true
+        return false
+    }
 
     /**
      * Преобразование в строку
      */
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        val str = StringBuilder()
+        if (im > 0) "+$im"
+        if (im < 0) "$im"
+        else ""
+        str.append("re" + im + "i")
+        return str.toString()
+    }
 }
