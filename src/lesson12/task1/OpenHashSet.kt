661a563158ac83312cc2723fdcfead6c3ced59b0
Amiiira <zuhir.as@edu.spbstu.ru>
2020-03-09 22:17:41 +0300

    Complex (done) & OpenHashSet (not complete, but I tried)

--- src/lesson12/task1/OpenHashSet.kt
+++ src/lesson12/task1/OpenHashSet.kt
@@ -2,6 +2,8 @@
 
 package lesson12.task1
 
+
+
 /**
  * Класс "хеш-таблица с открытой адресацией"
  *
@@ -16,6 +18,12 @@ package lesson12.task1
  * В конструктор хеш-таблицы передаётся её вместимость (максимальное количество элементов)
  */
 class OpenHashSet<T>(val capacity: Int) {
+    private var start: Node? = null
+
+    public class Node(
+        val value: String,
+        var next: Node? // next == null
+    )
 
     /**
      * Массив для хранения элементов хеш-таблицы
@@ -30,23 +38,43 @@ class OpenHashSet<T>(val capacity: Int) {
     /**
      * Признак пустоты
      */
-    fun isEmpty(): Boolean = TODO()
+    fun isEmpty(): Boolean {
+        if (elements.isEmpty()) return true
+        return false
+    }
 
     /**
      * Добавление элемента.
      * Вернуть true, если элемент был успешно добавлен,
      * или false, если такой элемент уже был в таблице, или превышена вместимость таблицы.
      */
-    fun add(element: T): Boolean = TODO()
+    fun add(element: T): Boolean {
+        if (element in elements || elements.size == capacity) return false
+        else start = Node(element.toString(), start)
+        return true
+    }
 
     /**
      * Проверка, входит ли заданный элемент в хеш-таблицу
      */
-    operator fun contains(element: T): Boolean = TODO()
+    operator fun contains(element: T): Boolean {
+        if (elements.contains(element)) return true
+        return false
+    }
 
     /**
      * Таблицы равны, если в них одинаковое количество элементов,
      * и любой элемент из второй таблицы входит также и в первую
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (other !is OpenHashSet<*>) return false
+        var elem = start
+        var otherElem = other.start
+        while (elem != null) {
+            if (otherElem == null || elem.value != otherElem.value) return false
+            elem = elem.next
+            otherElem = otherElem.next
+        }
+        return true
+    }
 }
